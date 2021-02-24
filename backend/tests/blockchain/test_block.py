6a1551c60c44a6fc4a2a3b8a163579c33c85fe14
Kieran Doyle <kieranhbdoyle@gmail.com>
2021-02-24 11:59:01 +0000

    proof of work difficult and tests

--- backend/tests/blockchain/test_block.py
+++ backend/tests/blockchain/test_block.py
@@ -1,4 +1,6 @@
+import time
 from backend.blockchain.block import Block, GENESIS_DATA
+from backend.config import MINE_RATE, SECONDS
 
 
 def test_mine_block():
@@ -9,6 +11,7 @@ def test_mine_block():
   assert isinstance(block, Block)
   assert block.data == data
   assert block.last_hash == last_block.hash
+  assert block.hash[0:block.difficulty] == '0' * block.difficulty
 
 
 def test_genesis():
@@ -19,3 +22,38 @@ def test_genesis():
   assert genesis.last_hash == GENESIS_DATA['last_hash']
   assert genesis.hash == GENESIS_DATA['hash']
   assert genesis.data == GENESIS_DATA['data']
+
+
+def test_quickly_mined_block():
+  last_block = Block.mine_block(Block.genesis(), 'foo')
+  mined_block = Block.mine_block(last_block, 'bar')
+
+  assert mined_block.difficulty == last_block.difficulty + 1
+
+
+def test_slowly_mined_block():
+  last_block = Block.mine_block(Block.genesis(), 'foo')
+
+  time.sleep(MINE_RATE / SECONDS)
+
+  mined_block = Block.mine_block(last_block, 'bar')
+
+  assert mined_block.difficulty == last_block.difficulty - 1
+
+def test_mined_block_difficulty_limits_at_1():
+  last_block = Block(
+    time.time_ns(),
+    'test_last_hash',
+    'test_hash',
+    'test_data',
+    1,
+    0
+  )
+
+  time.sleep(MINE_RATE / SECONDS)
+
+  mined_block = Block.mine_block(last_block, 'bar')
+
+  assert mined_block.difficulty == 1
+
+
